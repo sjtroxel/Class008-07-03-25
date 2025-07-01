bf820f111e22071e4a24ea5653f32203ebe9f739
sjtroxel <sjtroxel@alumni.iu.edu>
2025-07-01 18:28:22 -0500

    I am a postman-master general

--- pre-class-work-070325/controllers_and_routes_api/app/controllers/users_controller.rb
+++ pre-class-work-070325/controllers_and_routes_api/app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
   def create
     @user = User.new(name: params[:name], email: params[:email])
     if @user.save
@@ -7,4 +17,23 @@ class UsersController < ApplicationController
       render json: { error: "Unable to create user!" }
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(name: params[:name], email: params[:email])
+      render json: @user
+    else
+      render json: { error: "Unable to update user!" }
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      render json: { message: "Successfully deleted user." }
+    else
+      render json: { error: "Unable to delete user." }
+    end
+  end
 end
+
--- pre-class-work-070325/controllers_and_routes_api/config/routes.rb
+++ pre-class-work-070325/controllers_and_routes_api/config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :users, only: [:create]
+  resources :users, only: [:create, :index, :show, :update, :destroy]
 end
