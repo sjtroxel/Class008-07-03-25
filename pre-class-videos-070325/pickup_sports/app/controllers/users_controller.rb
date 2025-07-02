14644b4225efdb4ad3d6ccb03bd8d6f9773bb934
sjtroxel <sjtroxel@alumni.iu.edu>
2025-07-02 17:00:13 -0500

    finished the videos for Thursday 0703 with posts controller & routes

--- pre-class-videos-070325/pickup_sports/app/controllers/users_controller.rb
+++ pre-class-videos-070325/pickup_sports/app/controllers/users_controller.rb
@@ -37,6 +37,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def posts_index
+    user = User.find(params[:user_id])
+   
+    user_posts = user.posts
+
+    render json: user_posts, status: :ok
+  end
+
   private
 
   def set_user
