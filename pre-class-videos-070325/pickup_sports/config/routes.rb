d0ae18520ffd3c77f9085643723dd1e1ff33dcc4
sjtroxel <sjtroxel@alumni.iu.edu>
2025-07-02 15:14:30 -0500

    routes: get all, show one, create new, update existing, and destroy!

--- pre-class-videos-070325/pickup_sports/config/routes.rb
+++ pre-class-videos-070325/pickup_sports/config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # localhost:3000/users
+  get '/users', to: 'users#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # localhost:3000/users/1
+  get '/users/:id', to: 'users#show'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # localhost:3000/users
+  post '/users', to: 'users#create'
+
+  # localhost:3000/users/1
+  put '/users/:id', to: 'users#update'
+
+  # localhost:3000/users/1
+  delete '/users/:id', to: 'users#destroy'
 end
